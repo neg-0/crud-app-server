dc13f7de310d70fe6674aa0845ae16df39cf7101
Dustin Stringer <dustin.stringer@us.af.mil>
2023-06-21 08:32:58 -0400

    added dotenv and knex setup

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
